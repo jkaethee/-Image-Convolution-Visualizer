950f4b34f37648566debcaef08b6cb3137bb4fe0
jkaethee <47435952+jkaethee@users.noreply.github.com>
2020-05-17 18:48:44 -0400

    Initial version commit

--- app.py
+++ app.py
@@ -1,76 +1,100 @@
 import requests
 import string
 from flask import Flask, render_template, request, redirect, url_for, flash
-from flask_sqlalchemy import SQLAlchemy
+import numpy as np
+import cv2
+import os
+from PIL import Image
+from skimage.exposure import rescale_intensity
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///weather.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'thisisasecret'
-db = SQLAlchemy(app)
 
-
-class City(db.Model) :
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50),nullable=False)
-
-def get_weather_data(city):
-    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid=b21a2633ddaac750a77524f91fe104e7"
-    r = requests.get(url).json()
-    return r
 
 @app.route('/')
 def index_get():
-    cities = City.query.all()
+    # Remove the new image on refresh on main page
+    if os.path.exists('static/new/newImage.jpg'):
+        os.remove('static/new/newImage.jpg')
+    return render_template('main.html')
 
-    weather_data = []
+@app.route('/newImage/<name>')
+def add_newImg(name):
+    return render_template('output.html')
 
-    for city in cities:
-        r = get_weather_data(city.name)
-        weather = {
-            'city' : city.name,
-            'temperature' : r['main']['temp'],
-            'description' : r['weather'][0]['description'],
-            'icon' : r['weather'][0]['icon'],
-        }
-        weather_data.append(weather)
+@app.route('/convolutions', methods=["POST"])
+def apply_convolution():
+     # Remove the new image from directory on submit
+    if os.path.exists('static/new/newImage.jpg'):
+        os.remove('static/new/newImage.jpg')
 
-    return render_template('weather.html', weather_data=weather_data)
+    # Read original image in greyscale
+    originalImage = cv2.imread('static/iu.jpg',cv2.IMREAD_GRAYSCALE)
 
-@app.route('/', methods=['POST'])
-def index_post():
-    err_msg = ''
-    new_city = request.form.get('city')
-    new_city = new_city.lower()
-    new_city = string.capwords(new_city)
-    if new_city:
-        existing_city = City.query.filter_by(name=new_city).first()
-        
-        if not existing_city:
-            new_city_data = get_weather_data(new_city)
-            if new_city_data['cod'] == 200:
-                new_city_obj = City(name=new_city)
+    # getting the name from the POST request
+    name = request.form['convolution_type']
+    kernel = pick_convolution_type(name)
+    newImage = convolve(originalImage, kernel)
+    cv2.imwrite('static/newImage.jpg'.format(kernel),newImage)
+    return redirect(url_for('add_newImg', name=(name)))
 
-                db.session.add(new_city_obj)
-                db.session.commit()
-            else:
-                err_msg = 'That is not a valid city!'
-        else:
-            err_msg = 'City already exists in the database!'
+# KERNELS
+def Identity():
+    identity = np.array([[0,0,0],[0,1,0],[0,0,0]],dtype='int')
+    return identity
+def Sharpen():
+    sharpen = np.array([[0,-1,0],[-1,5,-1],[0,-1,0]],dtype='int')
+    return sharpen
+def EdgeDetection():
+    edgedetect = np.array([[-1,-1,-1],[-1,8,-1],[-1,-1,-1]],dtype='int')
+    return edgedetect
+def BoxBlur():
+    boxblur = np.array([[1,1,1],[1,1,1],[1,1,1]],dtype='int') / 9 
+    return boxblur
+def GaussianBlur():
+    gblur = np.array([[1,2,1],[2,4,2],[1,2,1]],dtype='int') / 16 
+    return gblur
 
-    if err_msg:
-        flash(err_msg, 'error')
-    else:
-        flash('City added successfully!', 'success')
+kernelDict = {
+    "Identity": Identity,
+    "Sharpen": Sharpen,
+    "EdgeDetection": EdgeDetection,
+    "BoxBlur": BoxBlur,
+    "GaussianBlur": GaussianBlur
+}
+# Function to access kernel dictionary for corresponding kernel
+def pick_convolution_type(name):
+    return kernelDict[name]()
 
-    return redirect(url_for('index_get'))
+# Convolution function
+def convolve(img,kernel):
+  # establish variables for the image height and length
+  (imgHeight,imgLength) = img.shape[:2]
 
-@app.route('/delete/<name>')
-def delete_city( name ):
-    city = City.query.filter_by(name=name).first()
-    db.session.delete(city)
-    db.session.commit()
+  # establish variables for the kernel height and length
+  (kHeight,kLength) = kernel.shape[:2]
 
-    flash(f'Successfully deleted { city.name }!', 'success')
-    return redirect(url_for('index_get'))
+  # A padded border is needed to deal with edge cases so
+  # we first calculate the thickness of the pad
+  pad = (kLength-1)//2
+
+  # Make a border with thickness of the aforementioned pad
+  img = cv2.copyMakeBorder(img,pad,pad,pad,pad,cv2.BORDER_REPLICATE)
+  
+  # establish numpy 2D array the size of the original image
+  final = np.zeros((imgHeight,imgLength),dtype='float32')
+
+  # apply convolutions from left to right, then up to down
+  for i in range(pad,imgHeight+pad):
+    for j in range(pad,imgLength+pad):
+      # define region of interest
+      region = img[i-pad:i+pad+1,j-pad:j+pad+1]
+      # sum up the new value of the center element
+      newVal = (region*kernel).sum()
+      final[i-pad,j-pad] = newVal
+
+  # normalize pixel value to lie in range 0-255    
+  final = rescale_intensity(final,in_range=(0,255))
+  final = (final * 255).astype("uint8")
+  # return manipulated image
+  return final
